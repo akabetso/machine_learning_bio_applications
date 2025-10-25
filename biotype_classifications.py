9f198b026f32e90cd0562a7a06a7b0447c92e4f2
Akabetso <nde.akabetso@gmail.com>
2025-10-25 17:12:23 +0200

    intro biotype.py

---
+++ biotype_classifications.py
@@ -0,0 +1,10 @@
+"""
+Implement deep learning to classify gene biotypes into coding and non coding genes
+
+Get coding and non coding genes from ncbi
+
+Train the multilayer peceptrons to recornize coding/non coding patterns
+
+evaluate on single cells transcripts
+"""
+
